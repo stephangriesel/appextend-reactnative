946d6a6af18a4e4d8e7c4d6013056bf74eff2f3e
Stephan <sgriesel@gmail.com>
2019-10-08 06:44:10 +0200

    basic set up & styling start

--- App.js
+++ App.js
@@ -1,10 +1,19 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={{ padding: 30 }}>
+      <View>
+        <TextInput
+          placeholder="Course Goal"
+          style={{ borderBottomColor: 'black', borderBottomWidth: 2, padding: 10 }}
+        />
+        <Button title="ADD" />
+      </View>
+      <View>
+
+      </View>
     </View>
   );
 }
