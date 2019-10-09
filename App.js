d47e1469cf44626a82987180e804eb515190ab60
Stephan <sgriesel@gmail.com>
2019-10-09 05:27:12 +0200

    updates styles, add flexbox

--- App.js
+++ App.js
@@ -4,10 +4,10 @@ import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 export default function App() {
   return (
     <View style={{ padding: 30 }}>
-      <View>
+      <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
         <TextInput
           placeholder="Course Goal"
-          style={{ borderBottomColor: 'black', borderBottomWidth: 2, padding: 10 }}
+          style={{ width: '80%', borderBottomColor: 'black', borderBottomWidth: 2, padding: 10 }}
         />
         <Button title="ADD" />
       </View>
