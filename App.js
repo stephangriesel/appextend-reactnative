bdda74ec09bcb0f71039268d1b4e282fb721a153
Stephan <sgriesel@gmail.com>
2019-10-11 06:31:39 +0200

    update styles, add state & add events

--- App.js
+++ App.js
@@ -1,18 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
+  const [enteredGoal, setEnteredGoal] = useState('');
+  const [courseGoals, setCourseGoals] = useState([]);
+
+  const goalInputHandler = (enteredText) => {
+    setEnteredGoal(enteredText)
+  };
+
+  const addGoalHandler = () => {
+    setCourseGoals(currentGoals => [...courseGoals, enteredGoal]);
+  }
+
   return (
     <View style={styles.screen}>
       <View style={styles.inputContainer}>
         <TextInput
           placeholder="Course Goal"
           style={styles.input}
+          onChangeText={goalInputHandler}
+          value={enteredGoal}
         />
-        <Button title="ADD" />
+        <Button title="ADD" onPress={addGoalHandler} />
       </View>
       <View>
-
       </View>
     </View>
   );
@@ -23,12 +35,13 @@ const styles = StyleSheet.create({
     padding: 50
   },
   inputContainer: {
-    width: '80%',
-    borderBottomColor: 'black',
-    borderBottomWidth: 2,
-    padding: 10
+    display: 'flex',
+    flexDirection: 'row'
   },
   input: {
-
+    width: '80%',
+    borderColor: 'black',
+    borderWidth: 1,
+    padding: 10
   }
 });
